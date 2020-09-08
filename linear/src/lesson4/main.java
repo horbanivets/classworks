d376c7767b29d3317b2d3d07451771da4dd21e5c
Horbanivets <Horbanivets@SCH3.LOCAL>
2020-09-08 15:47:34 +0300

    my first java project

---
+++ linear/src/lesson4/main.java
@@ -0,0 +1,12 @@
+package lesson4;
+
+public class main {
+    public static void main(String[] args) {
+        int x=5;
+        int y=6;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
