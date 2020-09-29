c6075711c6291bf331c3adc1c54d1f7e79ad0635
Horbanivets <horbanivets@SCH3.LOCAL>
2020-09-29 15:14:11 +0300

    horbanivets

--- linear/src/branching/pryamougolniki/Main.java
+++ linear/src/branching/pryamougolniki/Main.java
@@ -1,7 +1,54 @@
 package branching.pryamougolniki;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        int ax,ay,bx,by,cx,cy,dx,dy;
+        System.out.println("Введите координату х для точки А");
+        ax = s.nextInt();
+        System.out.println("Введите координату y для точки А");
+        ay = s.nextInt();
+        System.out.println("Введите координату х для точки B");
+        bx = s.nextInt();
+        System.out.println("Введите координату y для точки B");
+        by = s.nextInt();
+        System.out.println("Введите координату х для точки C");
+        cx = s.nextInt();
+        System.out.println("Введите координату y для точки C");
+        cy = s.nextInt();
+        System.out.println("Введите координату х для точки D");
+        dx = s.nextInt();
+        System.out.println("Введите координату y для точки D");
+        dy = s.nextInt();
+
+    if(ax==dx & ay==dy){
+            System.out.println("Совпадение в точке");
+        }
+    if(bx==cx & by==cy){
+        System.out.println("Совпадение в точке");
+        }
+    if (ax!=cx & ay!=cy & bx!=dx & by!=dy){
+        System.out.println("Перетин отсуствует");
+    }
+        if(ax==cx & by==cy){
+            System.out.println("Совпадение верхний отрезок");
+        }
+
+        if(ay==dy & bx==dx){
+            System.out.println("Совпадение нижний отрезок");
+        }
+
+        if(bx==cx & ay==cy){
+            System.out.println("Совпадение правий отрезок");
+        }
+
+        if(ax==dx & by==dy){
+            System.out.println("Совпадение левый отрезок");
+        }
+        if (ax==cx & ay==cy & bx==dx & by==dy){
+            System.out.println("Повне совпадение");
+        }
     }
 }
