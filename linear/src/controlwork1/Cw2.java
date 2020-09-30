7dbd6644a4f346d538a0eea7b06df48ff4b6779f
Horbanivets <horbanivets@SCH3.LOCAL>
2020-09-30 11:56:34 +0300

    horbanivets

--- linear/src/controlwork1/Cw2.java
+++ linear/src/controlwork1/Cw2.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Cw2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int x1,x2;
-        System.out.println("Введите десятие: ");
+        int x1,x2,x3;
+        System.out.println("Введите число");
         x1 = s.nextInt();
-        System.out.println("Введите еденици: ");
-        x2 = s.nextInt();
+        x2 = x1/10;
+        x3 = x1 - x2*10;
 
-        if(x1 + x2 >= 10){
+        if(x2 + x3 >= 10){
             System.out.println("Da");
         }else System.out.println("Net");
     }
